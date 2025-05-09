6c116612563f735c92ef453088a62c1b3399779c
Adriana Cardenas <adri.cardenas@utexas.edu>
2025-05-09 13:42:59 -0500

    chore(testing): first few tests added, jest config updates

--- src/components/LandingPage/LandingPage.test.tsx
+++ src/components/LandingPage/LandingPage.test.tsx
@@ -1,16 +1,26 @@
-// import React from 'react';
-// import {describe, expect} from '@jest/globals';
-// import {render} from '@testing-library/react';
-// import LandingPage from './LandingPage';
+import { render, screen } from '@testing-library/react'
+import LandingPage from './LandingPage'
 
-// const ANIMATION_TIME = 400;
-// const DISABLED = false;
+// Normally functioning parameters
+const ANIMATION_TIME = 4000
+const DISABLED = false
 
-// describe('Landing Page', () => {
-//     test('renders when disabled is false'), () => {
-//         render(<LandingPage animationAddress='' animationTime={400} disabled={false} />);
-//         const animationContainer = screen.getByClassName('animation-container');
-//         expect(animationContainer).toBeInTheDocument();
-//     }
+describe('Landing Page', () => {
+  test('renders when disabled is false'),
+    () => {
+      render(<LandingPage animationAddress="" animationTime={ANIMATION_TIME} disabled={DISABLED} />)
+      const animationContainer = screen.getByTestId('animation-container')
+      const animationContainerWrapper = screen.getByTestId('animation-wrapper-container')
+      expect(animationContainer).toBeInTheDocument()
+      expect(animationContainerWrapper).toBeInTheDocument()
+    }
 
-// })
+  test('does not render when disabled is true'),
+    () => {
+      render(<LandingPage animationAddress="" animationTime={ANIMATION_TIME} disabled={true} />)
+      const animationContainer = screen.queryByTestId('animation-container')
+      const animationContainerWrapper = screen.queryAllByTestId('animation-wrapper-container')
+      expect(animationContainer).toBeNull()
+      expect(animationContainerWrapper).toBeNull()
+    }
+})
